02461680f199e34df32da96243edd782faefb5ba
Himanshu Singh Tomar <himanshusinghtomar204@gmail.com>
2024-06-10 07:38:14 +0530

    more pattern questions

--- src/BasicOfJava/Patterns.java
+++ src/BasicOfJava/Patterns.java
@@ -9,7 +9,10 @@ public class Patterns {
 //        p.oneToFivePattern();
 //        p.oneOneToFiveFivePattern();
 //        p.reverseTriangle();
-        p.acuteAngleTriangle();
+//        p.erect_pyramid(5);
+//        p.inverted_pyramid(5);
+//        p.halfDiamondStarPattern(5);
+          p.binaryPattern(5);
     }
 
 //  square patter
@@ -74,21 +77,110 @@ public class Patterns {
         }
     }
 //    acute angle triangle
-    public void acuteAngleTriangle()
+    public void erect_pyramid(int N)
     {
-        for(int i =0;i<5;i++)
+        // This is the outer loop which will loop for the rows.
+        for(int i = 0;i<N;i++)
         {
-            for(int j =i;j<5;j++)
+            // For printing the spaces before stars in each row
+            for(int j =0;j<N-i-1;j++)
             {
-                for(int k =j;k<5;k++)
+                System.out.print(" ");
+            }
+
+            // For printing the stars in each row
+            for(int k =0;k<2*i+1;k++)
+            {
+                System.out.print("*");
+            }
+
+            // For printing the spaces after the stars in each row
+            for(int k =0;k<N-i-1;k++)
+            {
+                System.out.print(" ");
+            }
+
+            /*
+            * In the first row (i=0) there are 4 spaces, 1 star, then again 4 spaces.
+            * In the second row (i=1) there are 3 spaces, 3 stars,
+            *  then again 3 spaces so we can say
+            * that there are N-i-1 spaces, 2*i+1 stars, and then again N-i-1 spaces for each row where i is the row index.
+            *  We thus simply run 3 inner loops first for printing the spaces, then the stars, and then the spaces again.
+            *
+            * */
+
+
+            // As soon as the stars for each iteration are printed, we move to the
+            // next row and give a line break otherwise all stars
+            // would get printed in 1 line.
+            System.out.println();
+        }
+    }
+
+    public void inverted_pyramid(int N)
+    {
+        // this is outer loop for row
+        for(int i =0;i<N;i++)
+        {
+            for(int j =0;j<i;j++)
+            {
+                System.out.print(" ");
+            }
+
+            // For printing the stars in each row
+            for(int k =0;k< 2*N - (2*i+1);k++)
+            {
+                System.out.print("*");
+
+            }
+
+            // For printing the spaces after the stars in each row
+            for(int j =0;j<i;j++)
+            {
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+
+    public  void halfDiamondStarPattern(int N)
+    {
+        for(int i =0; i<N;i++)
+        {
+            for(int j = 0;j<i;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+            for(int i =0; i<N;i++)
+            {
+                for(int j = i;j<N;j++)
                 {
                     System.out.print("*");
                 }
                 System.out.println();
-//                if((5-i-1) == (2*i+1) )
-//                    continue;
-//                else
-//                    System.out.print("*");
+            }
+    }
+
+//    Binary
+    public void binaryPattern(int N)
+    {
+        int start = 1;
+        for(int i =0;i<N;i++)
+        {
+            // if the row index is even then 1 is printed first
+            // in that row.
+            if(i%2 ==0) start = 1;
+
+                // if odd, then the first 0 will be printed in that row.
+            else start = 0;
+
+            // We alternatively print 1's and 0's in each row by using
+            // the inner for loop.
+            for(int j=0;j<=i;j++){
+                System.out.print(start);
+                start = 1-start;
             }
             System.out.println();
         }
